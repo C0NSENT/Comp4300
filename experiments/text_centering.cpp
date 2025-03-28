b861e0559b5b3b8cbc6cb6cccfc539064d1ca8b3
consent_ <c0nsent@proton.me>
2025-03-28 20:30:26 +0300

    Тест

--- experiments/text_centering.cpp
+++ experiments/text_centering.cpp
@@ -2,4 +2,21 @@
 // Created by consent_ on 28-Mar-25.
 //
 
-#include "SFML/
+#include <iostream>
+#include <stdexcept>
+#include <filesystem>
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	constexpr auto screenSize = sf::Vector2u(1280, 720);
+	sf::RenderWindow window(sf::VideoMode(screenSize), "Text Centering");
+	window.setFramerateLimit(60);
+
+	std::cout<< std::filesystem::current_path().string() << std::endl;
+
+	sf::Font font("../Comp4300/stuff/font.ttf");
+
+
+}

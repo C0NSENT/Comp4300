b6825d5f475d2a1c5c6277e8e9d7b1d24b214b6a
consent_ <c0nsent@proton.me>
2025-05-25 20:23:15 +0300

    Update project name and adjust CMake workspace settings; implement Logger class structure

--- Assignment 2/ACTEPOID/Logger.cpp
+++ Assignment 2/ACTEPOID/Logger.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "Logger.hpp"
+
+#include <ctime>
+
+lrh::Logger & lrh::Logger::instance() {
+}
+
+void lrh::Logger::write(Level level, const std::string &message) {
+}
+
+lrh::Logger::Logger(const std::string &fileName) {
+}
+
+lrh::Logger::~Logger() {
+    //Закрываем файл и можно добавить
+    //Закрывающее сообщение лог файла
+    //TODO: Чекнуть как другие работяги закрывают лог файл
+}
+
+std::string lrh::Logger::createFileName(const std::string &currentDateTime) {
+}
+
+std::string lrh::Logger::currentDateTime() {
+    std::time_t currentTime = std::time(nullptr);
+    std::tm* localTime = std::localtime(&currentTime);
+
+
+}
+
--- Assignment 2/ACTEPOID/Logger.hpp
+++ Assignment 2/ACTEPOID/Logger.hpp
@@ -4,10 +4,43 @@
 
 #pragma once
 
+#include <fstream>
+#include <string>
+
+#include <chrono>
+
+
 namespace lrh
 {
+	/////////////////////////////////////////////////////////////////////
+	///
+	/// \brief Синглтон для логирования игры
+	///
+	/////////////////////////////////////////////////////////////////////
 	class Logger
 	{
 	public:
+		enum class Level : uint8_t {
+			Info = 0, Warning, Error
+		};
+
+		static Logger& instance();
+
+		void write(Level level, const std::string& message);
+
+		Logger(const Logger&) = delete;
+		Logger(Logger&&) = delete;
+		Logger& operator=(const Logger&) = delete;
+		Logger& operator=(Logger&&) = delete;
+
+	private:
+		explicit Logger(const std::string& fileName);
+		~Logger();
+		static std::string createFileName(const std::string& currentDateTime = currentDateTime());
+		static std::string currentDateTime();
+
+
+		std::string fileName;
+		std::ofstream fileStream;
 	};
 }
